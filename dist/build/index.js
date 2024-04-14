48d52ef11c329dad5a0d6303d7df6664362367cd
yolibork <yali@backslash.security>
2024-04-14 12:04:36 +0300

    he

--- dist/build/index.js
+++ dist/build/index.js
@@ -9,11 +9,11 @@ try {
     const sourceBranch = github.context.payload.pull_request.head.ref;
     const targetBranch = github.context.payload.pull_request.base.ref;
     const authToken = core.getInput('authToken');
-    const enforceBlock = core.getInput('enforceBlock') === 'true';
-    const isAll = core.getInput('allFindings') === 'true';
+    const enforceBlock = core.getBooleanInput('enforceBlock');
+    const isAll = core.getBooleanInput('allFindings');
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
-    console.log({ sourceBranch, targetBranch, authToken, enforceBlock, isAll });
+    console.log({ sourceBranch, targetBranch, authToken: authToken.length, enforceBlock, isAll });
 }
 catch (error) {
     core.setFailed(error.message);
--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,14 @@ try {
   const targetBranch: string = github.context.payload.pull_request.base.ref
 
   const authToken: string | undefined = core.getInput('authToken');
-  const enforceBlock: boolean = core.getInput('enforceBlock') === 'true';
-  const isAll: boolean = core.getInput('allFindings') === 'true';
+
+  const enforceBlock: boolean = core.getBooleanInput('enforceBlock');
+  const isAll: boolean = core.getBooleanInput('allFindings');
 
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 
-  console.log({sourceBranch, targetBranch, authToken, enforceBlock, isAll});
+  console.log({sourceBranch, targetBranch, authToken: authToken.length, enforceBlock, isAll});
   
 } catch (error) {
   core.setFailed(error.message);
