f52bdc21eee30e0de17d0f1319e68b872aba087e
yolibork <yali@backslash.security>
2025-03-23 12:49:02 +0200

    chmod

--- dist/index.js
+++ dist/index.js
@@ -70,7 +70,7 @@ function run() {
             }
             console.log(`Cli sha matches`);
             const commonArgs = `--authToken=${authToken} ${ignoreBlock ? `--warnOnly` : ''} --deltaScan=${prScan} --analyzedBranch=${analyzedBranch} --repositoryCloneUrl=${cloneUrl} --provider=${provider} --gitProviderOrganization=${organization} ${baselineBranch && `--baselineBranch=${baselineBranch} `} ${githubExtraInput} --outputPath=${outputPath}`;
-            const runCommand = `./${cliRunnerFileName} analyze ${commonArgs} ${pushToDashboard ? `--pushToDashboard` : ''}`;
+            const runCommand = `chmod 777 ${cliRunnerFileName} && ./${cliRunnerFileName} analyze ${commonArgs} ${pushToDashboard ? `--pushToDashboard` : ''}`;
             const child = (0, child_process_1.spawn)('bash', ['-c', runCommand], { stdio: ['inherit', 'pipe', 'pipe'] });
             child.stdout.on('data', (data) => {
                 console.log(data.toString('utf8'));
