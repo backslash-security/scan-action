316ca6473a70f37bca150681e5e4693a4f310b2c
David Hazani <129320934+david-backslash@users.noreply.github.com>
2025-05-21 10:24:04 +0300

    Fix BAC-8006: GHA | Local export  @david-backslash (#9)

--- dist/index.js
+++ dist/index.js
@@ -18,9 +18,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __nccwpck_require__(8167);
 const github = __nccwpck_require__(3459);
-const process = __nccwpck_require__(932);
-const fs_1 = __nccwpck_require__(9896);
 const crypto_1 = __nccwpck_require__(6982);
+const fs_1 = __nccwpck_require__(9896);
+const process = __nccwpck_require__(932);
 const child_process_1 = __nccwpck_require__(5317);
 const util_1 = __nccwpck_require__(9507);
 const cliRunnerFileName = 'run-cli.sh';
@@ -49,6 +49,7 @@ function run() {
             const isOnPremise = core.getBooleanInput('isOnPremise');
             const disablePrComments = core.getBooleanInput('disablePrComments');
             const pushToDashboard = core.getBooleanInput('pushToDashboard');
+            const localExport = core.getBooleanInput('localExport');
             const githubToken = core.getInput('githubToken');
             const cloneUrl = `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}.git`;
             const provider = isOnPremise ? 'github-enterprise-on-premise' : 'github';
@@ -56,10 +57,6 @@ function run() {
             const organization = github.context.payload.organization.login;
             if (repositoryName === undefined || analyzedBranch === undefined) {
                 return core.setFailed('Repo or branch not defined');
-            }
-            let githubExtraInput = '';
-            if (!disablePrComments) {
-                githubExtraInput = `--providerPrNumber=${github.context.issue.number} --providerAccessToken=${githubToken}`;
             }
             yield (0, util_1.downloadFile)(S3CLIUrl, cliRunnerFileName);
             yield (0, util_1.downloadFile)(S3CLIShaUrl, cliShaFileName);
@@ -69,8 +66,23 @@ function run() {
                 return core.setFailed(`Checksum failed, got ${fetchedHash} but expected ${generatedHash}`);
             }
             console.log(`Cli sha matches`);
-            const commonArgs = `--authToken=${authToken} ${ignoreBlock ? `--warnOnly` : ''} --deltaScan=${prScan} --analyzedBranch="${analyzedBranch}" --repositoryCloneUrl=${cloneUrl} --provider=${provider} --gitProviderOrganization=${organization} ${baselineBranch && `--baselineBranch="${baselineBranch}" `} ${githubExtraInput} --outputPath=${outputPath}`;
-            const runCommand = `bash ${cliRunnerFileName} analyze ${commonArgs} ${pushToDashboard ? `--pushToDashboard` : ''}`;
+            let analyzeArgs = `--authToken=${authToken} --deltaScan=${prScan} --analyzedBranch="${analyzedBranch}" --repositoryCloneUrl=${cloneUrl} --provider=${provider} --gitProviderOrganization=${organization} --outputPath=${outputPath}`;
+            if (!disablePrComments) {
+                analyzeArgs += ` --providerPrNumber=${github.context.issue.number} --providerAccessToken=${githubToken}`;
+            }
+            if (baselineBranch) {
+                analyzeArgs += ` --baselineBranch="${baselineBranch}"`;
+            }
+            if (ignoreBlock) {
+                analyzeArgs += ` --warnOnly`;
+            }
+            if (localExport) {
+                analyzeArgs += ` --outputPath=Backslash-scan-results/`;
+            }
+            if (pushToDashboard) {
+                analyzeArgs += ` --pushToDashboard`;
+            }
+            const runCommand = `bash ${cliRunnerFileName} analyze ${analyzeArgs}`;
             core.debug(`pushToDashboard: ${pushToDashboard}`);
             core.debug(`Running this command: ${runCommand}`);
             const child = (0, child_process_1.spawn)('bash', ['-c', runCommand], { stdio: ['inherit', 'pipe', 'pipe'] });
