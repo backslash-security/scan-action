904469ee06cd4cd0bd74e179b752ea385614db2d
amirgur <amir@backslash.security>
2026-04-13 18:27:22 +0300

    revert commit

--- dist/build/util.js
+++ dist/build/util.js
@@ -10,31 +10,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.downloadFile = void 0;
-const child_process_1 = require("child_process");
-/**
- * Downloads a file from S3 using the AWS CLI, which automatically picks up
- * the EC2 instance-role credentials on self-hosted runners.
- * Converts path-style HTTPS URLs (https://s3.amazonaws.com/bucket/key)
- * to s3://bucket/key URIs.
- */
+const https_1 = require("https");
+const fs_1 = require("fs");
 const downloadFile = (url, fileName) => __awaiter(void 0, void 0, void 0, function* () {
-    const s3Uri = url.replace(/^https:\/\/s3\.amazonaws\.com\//, 's3://');
     yield new Promise((resolve, reject) => {
-        var _a, _b;
-        const child = (0, child_process_1.spawn)('aws', ['s3', 'cp', s3Uri, fileName], {
-            stdio: ['inherit', 'pipe', 'pipe'],
-        });
-        (_a = child.stdout) === null || _a === void 0 ? void 0 : _a.on('data', (d) => process.stdout.write(d));
-        (_b = child.stderr) === null || _b === void 0 ? void 0 : _b.on('data', (d) => process.stderr.write(d));
-        child.on('close', (code) => {
-            if (code !== 0) {
-                reject(new Error(`aws s3 cp failed with exit code ${code} for ${s3Uri}`));
-            }
-            else {
-                resolve();
-            }
-        });
-        child.on('error', reject);
+        (0, https_1.get)(url, res => res.pipe((0, fs_1.createWriteStream)(fileName).on('finish', () => resolve('Finished downlaod'))
+            .on('error', (err) => reject(err))));
     });
 });
 exports.downloadFile = downloadFile;
